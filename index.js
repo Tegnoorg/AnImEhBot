f4115453ece7b2d9ec5ed5a3328d45121455f15f
Tegnoorg <tegnoor.gill@hotmail.com>
2023-03-11 13:29:37 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+require('dotenv/config')
+
+const client = new Client({
+    intents : [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+})
+
+client.on('ready', () => {
+    console.log('bot is ready')
+})
+
+client.login(process.env.TOKEN)
