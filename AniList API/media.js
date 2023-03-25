60426e2ea3b97a50dbd266ddf5c00a35628769d4
Tegnoorg <tegnoor.gill@hotmail.com>
2023-03-24 18:40:06 -0700

    media search set up

---
+++ AniList API/media.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+function request(searchQuery, type) {
+    const query = `
+      query ($search: String, $type: MediaType) {
+        Media(search: $search, type: $type) {
+          id
+          siteUrl
+        }
+      }
+    `;
+    const variables = {
+        search: searchQuery,
+        type: type,
+    };
+    const url = 'https://graphql.anilist.co/';
+    return axios.post(url, {
+            query: query,
+            variables: variables,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+        })
+        .then(response => response.data.data.Media.id)
+        .catch(error => {
+            console.error(error);
+            throw new Error('An error occurred while fetching the anime ID');
+        });
+}
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const { Client, GatewayIntentBits } = require('discord.js')
-require('dotenv/config')
+const {
+    Client,
+    GatewayIntentBits
+} = require('discord.js');
+require('dotenv').config();
+const media = require("./AniList API/media");
+const prefix = "!";
 
 const client = new Client({
-    intents : [
+    intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
@@ -10,7 +15,23 @@ const client = new Client({
 })
 
 client.on('ready', () => {
-    console.log('bot is ready')
+    console.log("bot is on");
+
 })
 
+client.on('message', (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(' ');
+        const command = args.shift().toLowerCase();
+        if (command === 'anime') {
+            const searchQuery = args.join(' ');
+            media.search(searchQuery, 'ANIME')
+                .then(animeID => console.log(`The ID for One Piece is ${animeID}`))
+                .catch(error => console.error(error));
+            return;
+        }
+        return;
+    }
+})
 client.login(process.env.TOKEN)
