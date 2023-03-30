8a98778607cbf8f6373f61b2598da0182346c0d8
PanesarV <vishav_panesar@hotmail.com>
2023-03-30 14:30:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-const {
-    Client,
-    GatewayIntentBits
-} = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
-const media = require("./AniList API/media");
-const prefix = "!";
+const media = require('./AniList API/media');
+const prefix = '!';
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
-})
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+let animeID; 
 
 client.on('ready', () => {
-    console.log("bot is on");
-    // media.request("one piece", 'ANIME')
-    //             .then(animeID => console.log(`The ID for One Piece is ${animeID}`))
-    //             .catch(error => console.error(error));
-
-})
+        console.log(`Logged in as ${client.user.tag}`);
+        const channel = client.channels.cache.get('1084221301046136926');
+        
+        if (channel && channel.viewable) {
+          console.log(`The bot can see the ${channel.name} channel`);
+        } else {
+          console.log(`The bot can't see the specified channel`);
+        }
+      
+      })
 
 client.on('message', (message) => {
-    if (message.author.bot) return;
-    if (message.content.startsWith(prefix)) {
-        const args = message.content.slice(prefix.length).trim().split(' ');
-        const command = args.shift().toLowerCase();
-        if (command === 'anime') {
-            const searchQuery = args.join(' ');
-            media.request(searchQuery, 'ANIME')
-                .then(animeID => console.log(`The ID for One Piece is ${animeID}`))
-                .catch(error => console.error(error));
-            return;
-        }
-        return;
+
+  console.log('Works');
+  message.reply('Hello')
+      .then(() => console.log('Replied to message "${message.content}"'))
+      .catch(console.error);
+
+    message.TextChannel.send("hi")
+  if (message.author.bot) return;
+  if (message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    if (command === 'anime') {
+      const searchQuery = args.join(' ');
+      media
+        .request(searchQuery, 'ANIME')
+        .then((id) => message.channel.send(`The ID for ${searchQuery} is ${id}`))
+        .catch((error) => console.error(error));
     }
-})
-client.login(process.env.TOKEN)
+    return;
+  }
+  message.channel.send(`The ID for One Piece is ${animeID}`);
+});
+
+client.login(process.env.TOKEN);
