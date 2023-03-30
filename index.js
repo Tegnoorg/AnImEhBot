8c94b104f4e5e5f5f99f118aa06e200a49336bb4
Tegnoorg <tegnoor.gill@hotmail.com>
2023-03-30 14:23:21 -0700

    bot replying to messages with correct info

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const {
     Client,
-    GatewayIntentBits
+    GatewayIntentBits,
+    Events
 } = require('discord.js');
 require('dotenv').config();
 const media = require("./AniList API/media");
 const prefix = "!";
 
+
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
-})
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 client.on('ready', () => {
+    console.log(`${client.user.username} has logged in.`);
     console.log("bot is on");
+    media.request("one piece", 'ANIME')
+                .then(animeID => console.log(`The ID for One Piece is ${animeID}`))
+                .catch(error => console.error(error));
+
+})
+
+client.on(Events.MessageCreate, message => {
+    // console.log(message.content)
+    // message.channel.send("hi");
+    // console.log("hi");
+    // console.log("bot is on");
     // media.request("one piece", 'ANIME')
     //             .then(animeID => console.log(`The ID for One Piece is ${animeID}`))
     //             .catch(error => console.error(error));
-
-})
-
-client.on('message', (message) => {
     if (message.author.bot) return;
     if (message.content.startsWith(prefix)) {
         const args = message.content.slice(prefix.length).trim().split(' ');
@@ -30,7 +41,7 @@ client.on('message', (message) => {
         if (command === 'anime') {
             const searchQuery = args.join(' ');
             media.request(searchQuery, 'ANIME')
-                .then(animeID => console.log(`The ID for One Piece is ${animeID}`))
+                .then(animeID => message.reply(`The ID for One Piece is ${animeID}`))
                 .catch(error => console.error(error));
             return;
         }
@@ -38,3 +49,14 @@ client.on('message', (message) => {
     }
 })
 client.login(process.env.TOKEN)
+
+
+
+
+
+
+
+
+
+
+
