563e7dd51e53b774677998eadcb04d3a0f914299
Tegnoorg <tegnoor.gill@hotmail.com>
2023-05-26 19:29:49 -0700

    random anime suggestion

--- AniList API/suggestions.js
+++ AniList API/suggestions.js
@@ -1,49 +1,10 @@
-const axios = require('axios');
 
-async function request(searchQuery, type) {
-  try {
-    // Get similar anime recommendations by ID
-    const recommendationsResponse = await axios.post('https://graphql.anilist.co', {
-      query: `
-      query ($search: String) {
-        Page {
-          media(search: $search, type: ANIME) {
-            recommendations {
-              edges {
-                node {
-                  mediaRecommendation {
-                    title {
-                      romaji
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      `,
-      variables : {
-        search: searchQuery,
-        type: type
-    }
-    });
 
-    // Get the recommendations from the response
-    const recommendations = recommendationsResponse.data.data.Recommendation;
-    const result = recommendations.map(rec => ({
-      recommendations: rec.node.mediaRecommendation.page,
-    }));
-
-    // Sort recommendations by rating in descending order
-    console.log(recommendations);
-
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
+async function request() {
+  const randomNumber = Math.floor(Math.random() * 10000) + 1;
+  return randomNumber;
 }
-  request("one piece", 'ANIME');
+console.log(request());
 module.exports = {
   request: request
 };
